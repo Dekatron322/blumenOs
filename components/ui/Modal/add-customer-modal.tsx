38a87637325f482288a5d87f5d5f119768a20c95
Cygnux X-1 <62252129+Dekatron322@users.noreply.github.com>
2025-11-01 15:11:37 +0100

    style: reorder React hook imports alphabetically

--- components/ui/Modal/add-customer-modal.tsx
+++ components/ui/Modal/add-customer-modal.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useRef } from "react"
+import React, { useRef, useState } from "react"
 import { motion } from "framer-motion"
 import CloseIcon from "public/close-icon"
 
