73af00d22a4839c65a81082e47eeaa55b71d5596
Cygnux X-1 <62252129+Dekatron322@users.noreply.github.com>
2025-11-10 22:56:21 +0100

    refactor: remove unused customerSlice and clean up store configuration
    - Deleted commented-out customerSlice.ts file containing Customer API definitions
    - Removed customerApi references from Redux store middleware configuration

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -2,21 +2,15 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authReducer from "./authSlice"
 
-// import { customerApi } from "./customerSlice"
 import { adminApi } from "./adminSlice"
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
 
-    // [customerApi.reducerPath]: customerApi.reducer,
     [adminApi.reducerPath]: adminApi.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
-      // .concat(customerApi.middleware)
-
-      .concat(adminApi.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(adminApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
