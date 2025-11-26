a73d9dad66521a7d40107a92d71d49547135d7d0
Cygnux X-1 <62252129+Dekatron322@users.noreply.github.com>
2025-11-26 09:21:03 +0100

    refactor: alphabetize imports in outage management tabs
    - Reorder redux imports in outages tab (AppDispatch, RootState)
    - Reorder redux imports in outages tab (Outage as ApiOutage, fetchOutages, OutageRequestParams)
    - Reorder react imports in reports tab (useEffect, useRef, useState)
    ```

--- components/OutageManagementInfo/ReportsTab.tsx
+++ components/OutageManagementInfo/ReportsTab.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useState, useRef } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { AnimatePresence, motion } from "framer-motion"
 import { RxCaretSort, RxDotsVertical } from "react-icons/rx"
 import { MdOutlineArrowBackIosNew, MdOutlineArrowForwardIos, MdOutlineCheckBoxOutlineBlank } from "react-icons/md"
