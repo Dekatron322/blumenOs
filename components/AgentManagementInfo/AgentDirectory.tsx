70350070407f02b9930314118844d7498b043992
Cygnux X-1 <62252129+Dekatron322@users.noreply.github.com>
2025-12-01 14:06:28 +0100

    refactor: alphabetize imports and remove unused icon imports in agent directory
    - Alphabetize agentSlice imports (clearAgents, fetchAgents, type Agent)
    - Remove unused icon imports (CycleIcon, DateIcon, RevenueGeneratedIcon, StatusIcon)

--- components/AgentManagementInfo/AgentDirectory.tsx
+++ components/AgentManagementInfo/AgentDirectory.tsx
@@ -4,21 +4,17 @@ import { SearchModule } from "components/ui/Search/search-module"
 import {
   AddAgentIcon,
   BillsIcon,
-  CycleIcon,
-  DateIcon,
   FloatIcon,
   MapIcon,
   PerformanceIcon,
   PhoneIcon,
   RateIcon,
-  RevenueGeneratedIcon,
   RouteIcon,
-  StatusIcon,
   TargetIcon,
   UserIcon,
 } from "components/Icons/Icons"
 import { useAppDispatch, useAppSelector } from "lib/hooks/useRedux"
-import { fetchAgents, clearAgents, type Agent } from "lib/redux/agentSlice"
+import { clearAgents, fetchAgents, type Agent } from "lib/redux/agentSlice"
 
 const CyclesIcon = () => (
   <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
